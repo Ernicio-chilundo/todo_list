0a90b00ee9fb65218259273555bbaf3579cf5f22
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-07-04 16:19:50 +0200

    add

--- todo.js
+++ todo.js
@@ -5,13 +5,13 @@ let banco =[
     {"tarefa":"netflix", "status": "checked"}
 ]
 
-const criarItem =(tarefa,status)=>{
+const criarItem =(tarefa,status,indice)=>{
     const item = document.createElement("label")
     item.classList.add("todo_item")
     item.innerHTML = `
-        <input type ="checkbox" ${status}>
+        <input type ="checkbox" ${status} data-indice=${indice}>
         <div>${tarefa}</div>
-        <input type="button" value="x">
+        <input type="button" value="x" data-indice=${indice}>
     `
     document.getElementById("todoList").appendChild(item)
 }
@@ -24,8 +24,39 @@ const limparTarefas = ()=>{
 
 const atualizarTela = ()=>{
     limparTarefas()
-    banco.forEach(item => criarItem(item.tarefa, item.status))
+    banco.forEach((item,indice) => criarItem(item.tarefa, item.status, indice))
 }
-document.getElementById("newItem").addEventListener("keypress",)
+const inserirItem =(evento)=>{
+    const tecla =evento.key
+    const texto = evento.target.value
+    if(tecla == "Enter"){
+        banco.push({"tarefa": texto, "status":""})
+        atualizarTela()
+        evento.target.value=""
+    }
+}
+
+const removerItem = (indice)=>{
+    banco.splice(indice, 1)
+    atualizarTela()
+}
+
+const atualizarItem=(indice)=>{
+    banco[indice].status = banco[indice].status == ''? "checked": ""
+    atualizarTela()
+}
+
+const clickItem =(evento)=>{
+    const elemento = evento.target
+    if(elemento.type === "button"){
+       const indice = elemento.dataset.indice
+        removerItem(indice)
+    }else if(elemento.type === "checkbox"){
+        const indice = elemento.dataset.indice
+        atualizarItem(indice)
+    }
+}
+document.getElementById("newItem").addEventListener("keypress",inserirItem)
+document.getElementById("todoList").addEventListener("click",clickItem)
 
 atualizarTela()
